daec50206032cba1efe1ed5a62d43847a0e30822
Luccas F. Oliveira <luccas.felippe@outlook.com>
2015-12-02 07:51:20 -0200

    Commit inicial

---
+++ src/ex_gitbranch/Ex_gitbranch.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex_gitbranch;
+
+/**
+ *
+ * @author Luccas
+ */
+public class Ex_gitbranch {
+	/**
+	 * Imprime algum texto básico -- testando o git
+	 * msg a ser imprimida "GIT É MASSA"
+	 */
+	private static void method1() {
+		System.out.println("GIT É MASSA");
+	}
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
